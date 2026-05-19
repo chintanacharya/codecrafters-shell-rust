5acfc9c3c01d1a16c1267079dd9f36ecb4e4de62
Chintan <acharya.chintan96@gmail.com>
2026-05-19 10:22:46 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut raw_cmd = String::new();
+    io::stdin().read_line(&mut raw_cmd).unwrap();
+    let trimmed_cmd = raw_cmd.trim_end();
+
+    println!("{trimmed_cmd}: command not found");
 }
