788b4f70e9795f67dafb0fe3e960e651858da35b
Chintan <acharya.chintan96@gmail.com>
2026-05-19 10:30:19 +0530

    REPL

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,22 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    repl();
+}
 
-    let mut raw_cmd = String::new();
-    io::stdin().read_line(&mut raw_cmd).unwrap();
-    let trimmed_cmd = raw_cmd.trim_end();
+fn repl() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    println!("{trimmed_cmd}: command not found");
+        let mut raw_cmd = String::new();
+        io::stdin().read_line(&mut raw_cmd).unwrap();
+        let trimmed_cmd = raw_cmd.trim_end();
+
+        process_cmd(&trimmed_cmd);
+    }
+}
+
+fn process_cmd(cmd: &str) {
+    println!("{cmd}: command not found");
 }
