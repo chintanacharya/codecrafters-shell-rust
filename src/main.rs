36722840800f089dd55882645d6b1074fcdc0251
Chintan <acharya.chintan96@gmail.com>
2026-05-19 10:54:58 +0530

    Parse command and line separately

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,26 @@ fn repl() {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut raw_cmd = String::new();
-        io::stdin().read_line(&mut raw_cmd).unwrap();
-        let trimmed_cmd = raw_cmd.trim_end();
+        let mut raw_line = String::new();
+        io::stdin().read_line(&mut raw_line).unwrap();
+        let trimmed_line = raw_line.trim_end();
 
-        process_cmd(&trimmed_cmd);
+        process_line(&trimmed_line);
     }
 }
 
-fn process_cmd(cmd: &str) {
+fn process_line(line: &str) {
+    let cmd_term_index = line.find(|c: char| c.is_whitespace());
+    match cmd_term_index {
+        Some(index) => process_cmd(&line[0..index], &line[index..].trim()),
+        None => process_cmd(line, ""),
+    }
+}
+
+fn process_cmd(cmd: &str, line: &str) {
     match cmd {
         "exit" => std::process::exit(0),
+        "echo" => println!("{}", line),
         _ => println!("{cmd}: command not found"),
     }
 }
